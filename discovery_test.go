a2cc0479f8b81feb5b21660de536f91db186192e
ChronosX88 <chronosx88@gmail.com>
2020-10-28 02:54:08 +0400

    Implement basic discovery_test unittest

---
+++ discovery_test.go
@@ -0,0 +1,102 @@
+package pex
+
+import (
+	"context"
+	"fmt"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/discovery"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/peer"
+	ma "github.com/multiformats/go-multiaddr"
+	"github.com/sirupsen/logrus"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestDiscovery(t *testing.T) {
+	logrus.SetLevel(logrus.DebugLevel)
+
+	// setup new libp2p hosts - one bootstrap and two regular peers
+	rand.Seed(time.Now().UnixNano())
+	port1 := rand.Intn(100) + 10000
+	port2 := port1 + 1
+	port3 := port2 + 1
+
+	bNode := makeNode(port1)
+	_, err := NewPEXDiscovery(bNode, nil, 1*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Bootstrap ID: %s", bNode.ID())
+
+	rNode1 := makeNode(port2)
+	t.Logf("Node 1 ID: %s", rNode1.ID())
+	rNode2 := makeNode(port3)
+	t.Logf("Node 2 ID: %s", rNode2.ID())
+
+	bootstrapMultiaddrs, err := peer.AddrInfoToP2pAddrs(&peer.AddrInfo{bNode.ID(), bNode.Addrs()})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	d1, err := NewPEXDiscovery(rNode1, []ma.Multiaddr{bootstrapMultiaddrs[0]}, 1*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	d2, err := NewPEXDiscovery(rNode2, []ma.Multiaddr{bootstrapMultiaddrs[0]}, 1*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// setup new peers listener
+	go func() {
+		newPeers, err := d1.FindPeers(context.TODO(), "test", discovery.Limit(10))
+		if err != nil {
+			t.Fatal(err)
+		}
+		for {
+			p := <-newPeers
+			t.Logf("d1 new peer: %s", p)
+		}
+	}()
+
+	go func() {
+		newPeers, err := d2.FindPeers(context.TODO(), "test", discovery.Limit(10))
+		if err != nil {
+			t.Fatal(err)
+		}
+		for {
+			p := <-newPeers
+			t.Logf("d2 new peer: %s", p)
+		}
+	}()
+
+	// do advertising for regular peers
+	_, err = d1.Advertise(context.TODO(), "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = d2.Advertise(context.TODO(), "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	select {}
+}
+
+func makeNode(port int) host.Host {
+	// Ignoring most errors for brevity
+	// See echo example for more details and better implementation
+	priv, _, _ := crypto.GenerateKeyPair(crypto.Secp256k1, 256)
+	listen, _ := ma.NewMultiaddr(fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", port))
+	host, _ := libp2p.New(
+		context.Background(),
+		libp2p.ListenAddrs(listen),
+		libp2p.Identity(priv),
+	)
+
+	return host
+}
